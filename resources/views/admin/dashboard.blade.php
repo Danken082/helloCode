6d0f27b2ac76e3d06d9464e05c63515426a84f70
DanRontale <rontaledankeneth@gmail.com>
2025-05-22 13:39:43 +0800

    hello

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -160,7 +160,7 @@ select.form-select {
 </div>
 
 <div class="col-md-4">
-  <a href="{{ route('admin)}}" class="text-decoration-none">
+  <a href="{{ route('admin')}}" class="text-decoration-none">
     <div class="card text-dark bg-white mb-4 shadow rounded-4 border-0 hover-scale">
       <div class="card-body text-center fw-bold">
         <h5 class="card-title">Total Admin</h5>
