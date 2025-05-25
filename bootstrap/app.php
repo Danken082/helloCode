77b61dc5064a0389a0e803b8b08de627d567943b
DanRontale <rontaledankeneth@gmail.com>
2025-05-25 23:37:41 +0800

    psuh

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
         ]);
 
-        $middleware->redirectGuestsTo(fn (Request $request) => '/');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
