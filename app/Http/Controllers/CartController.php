4765e26aab3428f63e7fb6ccfda2e7e7fc4d15a4
DanRontale <rontaledankeneth@gmail.com>
2025-05-27 20:23:39 +0800

    tes

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,11 +16,13 @@ class CartController extends Controller
             ->where('userID', $userID)
             ->get();
 
-            $totalPrice = $cartItems->sum(function ($item) {
-                return $item->product->productPrice * $item->quantity;
-            });
+        //     $totalPrice = $cartItems->sum(function ($item) {
+        //         return $item->product-> * $item->quantity;
+        //     });
 
-        return view('user.cart', compact('cartItems', 'totalPrice'));
+        // return view('user.cart', compact('cartItems', 'totalPrice'));
+
+        var_dump($cartItems);
     }
 
     public function updateQuantity(Request $request, $id)
